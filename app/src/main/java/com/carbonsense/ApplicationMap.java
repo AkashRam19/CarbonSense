921669becd3913e3e27078836d055f83b06c93f7
John Baldwin <baldwinjr.j@gmail.com>
2020-02-02 03:58:26 -0500

    Commit what I've got oof

---
+++ app/src/main/java/com/carbonsense/ApplicationMap.java
@@ -0,0 +1,5 @@
+package com.carbonsense;
+
+class ApplicationMap {
+    public static final int CAMERA_PIC_REQUEST = 23;
+}
---
+++ app/src/main/java/com/carbonsense/MainActivity.java
@@ -0,0 +1,66 @@
+package com.carbonsense;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OnClickListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listener = new OnClickListener() {
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.camera_button:
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, ApplicationMap.CAMERA_PIC_REQUEST);
+                        setContentView(R.layout.activity_trash_capture);
+                        break;
+
+                }
+                ;
+            }
+        };
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
